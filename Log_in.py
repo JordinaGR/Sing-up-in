870a0bcea17ceb2c2e93d45616ad977376819806
Jordina Gavalda <jordinagavalda@gmail.com>
2020-05-16 18:01:44 +0200

    password update

--- Log_in.py
+++ Log_in.py
@@ -1,7 +1,7 @@
 import hashlib
 import mysql.connector
 import os
-
+import getpass
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -13,24 +13,29 @@ my_cursor = mydb.cursor()
 
 def log_in():
     email = str(input('Email: '))
-    passw = str(input('Password: '))
-    read = str(input('Do you want to read or change your text?(r/c)'))
+    passw = getpass.getpass()
     md5passw = hashlib.md5(passw.encode())
     passwdb = md5passw.hexdigest()
 
     my_cursor.execute(f'SELECT info FROM login_data WHERE email = "{email}" AND pass = "{passwdb}";')
     p = my_cursor.fetchone()
-    if read.lower() == 'r':
-        for i in p:
-            print(i)
-    elif read.lower() == 'c':
-        print('\nThis is the last thing that you typed:')
-        for i in p:
-            print(i)
-        info = str(input('\nWhat do you want to type now?\n'))
-        my_cursor.execute(f"UPDATE `log_in`.`login_data` SET `info` = '{info}' WHERE (`email` = '{email}');")
-        mydb.commit()
-        print('Your text has been changed.')
+
+    try:
+        read = str(input('Do you want to read or change your text?(r/c)'))
+        if read.lower() == 'r':
+            for i in p:
+                print(i)
+        elif read.lower() == 'c':
+            print('\nThis is the last thing that you typed:')
+            for i in p:
+                print(i)
+            info = str(input('\nWhat do you want to type now?\n'))
+            my_cursor.execute(f"UPDATE `log_in`.`login_data` SET `info` = '{info}' WHERE (`email` = '{email}');")
+            mydb.commit()
+            print('Your text has been changed.')
+    except:
+        print('wrong email or password')
+        quit()
     quit()
 
 def sing_up():
@@ -38,20 +43,25 @@ def sing_up():
     name = str(input('Name: '))
     last = str(input('Last: '))
     email = str(input('Email: '))
-    passw = str(input('Password: '))
+    passw = getpass.getpass()
+    passw1 = getpass.getpass(prompt='Confirm password: ')
 
-    info = f'Type something, {name}'
+    if passw == passw1:
+        info = f'Type something, {name}'
 
-    print('loading... ')
+        print('loading... ')
 
-    try:
-        sqlformula = "INSERT INTO login_data (nom, cognom, email, pass, username, info) VALUES (%s, %s, %s, md5(%s), " \
-                     "%s, %s) "
-        dades = (name, last, email, passw, user, info)
-        my_cursor.execute(sqlformula, dades)
-        mydb.commit()
-    except:
-        print('Email or username already in use')
+        try:
+            sqlformula = "INSERT INTO login_data (nom, cognom, email, pass, username, info) VALUES (%s, %s, %s, md5(%s), " \
+                         "%s, %s) "
+            dades = (name, last, email, passw, user, info)
+            my_cursor.execute(sqlformula, dades)
+            mydb.commit()
+        except:
+            print('Email or username already in use')
+    else:
+        print('Passwords doesn\'t match, try again')
+        sing_up()
 
 log_sing = input('Log in or sing up? (log/sing)')
 
